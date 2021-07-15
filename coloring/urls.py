8d49bda217ece0a9d4d576278fdcaf9d53982cbe
joewychen <powcca@berkeley.edu>
2021-07-15 00:17:32 -0700

    added main and gallery pages

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('demo', views.index, name='demo'),
     path('new_interaction', views.interaction, name='new_interaction'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('gallery', views.gallery, name='gallery'),
+    path('new_interaction_trig', views.interaction_trig, name='new_interaction_trig')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -5,3 +5,9 @@ def index(request):
 
 def interaction(request):
     return render(request, 'coloring/new_interaction.html')
+
+def gallery(request):
+    return render(request, 'coloring/gallery.html')
+
+def interaction_trig(request):
+    return render(request, 'coloring/new_interaction_trig.html')
